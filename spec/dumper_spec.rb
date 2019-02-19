17e9b741e6bdfd775ddf7a1caaddf37f15e835a5
david942j <david942j@gmail.com>
2019-02-18 20:56:09 -0800

    Test exit code to reach full coverage

--- spec/dumper_spec.rb
+++ spec/dumper_spec.rb
@@ -61,4 +61,10 @@ describe SeccompTools::Dumper do
       end
     end
   end
+
+  it 'should output warning and exit(1)' do
+    allow(SeccompTools::Ptrace).to receive(:traceme_and_stop)
+    expect { described_class.__send__(:handle_child, 'no_such_binary') }
+      .to terminate.with_code(1).and output("Failed to execute no_such_binary\n").to_stderr
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,3 +6,41 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
 SimpleCov.start do
   add_filter '/spec/'
 end
+
+RSpec::Matchers.define :terminate do
+  actual = nil
+
+  def supports_block_expectations?
+    true
+  end
+
+  match do |block|
+    begin
+      block.call
+    rescue SystemExit => e
+      actual = e.status
+    end
+    actual && (actual == status_code)
+  end
+
+  chain :with_code do |status_code|
+    @status_code = status_code
+  end
+
+  failure_message do |_block|
+    "expected block to call exit(#{status_code}) but exit" +
+      (actual.nil? ? ' not called' : "(#{actual}) was called")
+  end
+
+  failure_message_when_negated do |_block|
+    "expected block not to call exit(#{status_code})"
+  end
+
+  description do
+    "expect block to call exit(#{status_code})"
+  end
+
+  def status_code
+    @status_code ||= 0
+  end
+end
